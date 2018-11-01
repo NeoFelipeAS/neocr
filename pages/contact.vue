725af7baae484f9cd968c3275d7f2f47e1646de6
Neo <i_felialva@hotmail.com>
2018-11-01 01:16:47 -0600

    Primer commit portafolio estatico Nuxt.js

---
+++ pages/contact.vue
@@ -0,0 +1,12 @@
+<!-- pages/contact.vue -->
+
+<template>
+  <section class="section is-medium">
+    <div class="container has-text-centered">
+      <h1 class="title">Contactame</h1>
+      <p>
+        Me puede encontrar en las redes sociales.
+      </p>
+    </div>
+  </section>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<!-- pages/index.vue -->
+
+<template>
+  <section class="section is-large">
+    <div class="container has-text-centered">
+      <h1 class="title">Hola, me llamo Felipe Alvarado Solano.</h1>
+      <h2 class="subtitle">Soy un programador de Costa Rica.</h2>
+      <figure class="image image is-16by9	">
+        <img src="~assets/Sketch.png">
+      </figure>
+    </div>
+  </section>
+</template>
